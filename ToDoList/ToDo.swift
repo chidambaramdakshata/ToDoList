0f93d36604a881cb64b1eaf356b2db37a32d213f
Dakshata Chidambaram <chidambaramdakshata@gmail.com>
2022-08-02 10:23:29 -0700

    Initial Commit

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Dakshata Chidambaram on 8/2/22.
+//
+
+import UIKit
+
+class ToDo {
+var name = ""
+    var important = false
+    
+    
+    
+}
